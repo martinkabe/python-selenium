2c440b108e31eede5ea74ab5dcfef1eead55ff86
martinkabe <martinkabe@seznam.cz>
2024-10-10 22:16:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def process_json():
         return 'Content-Type not supported!'
 
     options = webdriver.ChromeOptions()
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-infobars")
